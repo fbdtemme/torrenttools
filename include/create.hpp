8bc98ae34e0b548fa7ce67b2aaa093bf725973df
fbdtemme <floriandetemmerman@gmail.com>
2021-01-23 08:47:41 +0100

    Add --creation-date, --created-by, --stdout, and fix various options.

--- include/create.hpp
+++ include/create.hpp
@@ -4,6 +4,7 @@
 #include <optional>
 #include <cstdint>
 #include <filesystem>
+#include <chrono>
 #include <dottorrent/metafile.hpp>
 
 #include "config.hpp"
@@ -12,39 +13,36 @@
 #include "dottorrent/dht_node.hpp"
 #include "info.hpp"
 
+namespace { namespace fs = std::filesystem; }
+
+// forward declarations
 namespace CLI { class App; }
-
-namespace  {
-namespace fs = std::filesystem;
-}
-
-// forward declare
-namespace torrenttools {
-class file_matcher;
-}
+namespace torrenttools { class file_matcher; }
 
 struct create_app_options
 {
     std::filesystem::path target;
     std::optional<std::filesystem::path> destination;
+    std::optional<bool> write_to_stdout = false;
+    dottorrent::protocol protocol_version = dottorrent::protocol::v1;
+    std::optional<std::size_t> piece_size;
+    std::unordered_set<dottorrent::hash_function> checksums = {};
+    std::vector<std::vector<std::string>> announce_list;
+    std::vector<std::string> web_seeds;
+    std::vector<dottorrent::dht_node> dht_nodes;
     std::vector<std::string> include_patterns;
     std::vector<std::string> exclude_patterns;
-    std::vector<std::vector<std::string>> announce_list;
+    bool include_hidden_files = false;
     std::optional<std::string> comment;
     std::optional<std::string> source;
     std::optional<std::string> name;
-    std::vector<std::string> web_seeds;
-    std::vector<dottorrent::dht_node> dht_nodes;
-    std::optional<std::size_t> piece_size;
-    std::uint8_t threads = 4;
-    std::optional<std::size_t> io_block_size;
-    // determine private flag based on tracker or override with --private
     std::optional<bool> is_private = std::nullopt;
     bool set_created_by = true;
+    std::optional<std::string> created_by {};
     bool set_creation_date = true;
-    bool include_hidden_files = false;
-    std::unordered_set<dottorrent::hash_function> checksums = {};
-    dottorrent::protocol protocol_version = dottorrent::protocol::v1;
+    std::optional<std::chrono::system_clock::time_point> creation_date = std::nullopt;
+    std::uint8_t threads = 4;
+    std::optional<std::size_t> io_block_size;
 
 };
 
@@ -54,9 +52,9 @@ void configure_matcher(torrenttools::file_matcher& matcher, const create_app_opt
 
 void run_create_app(const create_app_options& options);
 
-void run_with_progress(dottorrent::storage_hasher& hasher, const dottorrent::metafile& m);
+void run_with_progress(std::ostream& os, dottorrent::storage_hasher& hasher, const dottorrent::metafile& m);
 
-void print_creation_statistics(const dottorrent::metafile& m, std::chrono::system_clock::duration duration);
+void print_creation_statistics(std::ostream& os, const dottorrent::metafile& m, std::chrono::system_clock::duration duration);
 
 void set_files(dottorrent::metafile& m, const create_app_options& options);
 
--- src/argument_parsers.cpp
+++ src/argument_parsers.cpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <charconv>
 #include <unordered_set>
+#include <chrono>
+#include <date/date.h>
 
 #include "dottorrent/serialization/path.hpp"
 
@@ -21,9 +23,30 @@ namespace dt = dottorrent;
 using namespace dottorrent::literals;
 static std::string err_msg = "Invalid value {} for option {}: {}.";
 
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
 
 std::unordered_set<dottorrent::hash_function> checksum_transformer(const std::vector<std::string>& s)
 {
+    // use a set to filter out duplicates
     std::unordered_set<dt::hash_function> res {};
 
     for (const auto& name : s) {
@@ -101,7 +124,7 @@ std::vector<dottorrent::dht_node> dht_node_transformer(const std::vector<std::st
     for (const auto& node : s) {
         auto it = rng::find(node, ':');
         if (it == node.end()) {
-            throw std::invalid_argument("Invalid dht node: invalid port number");
+            throw std::invalid_argument("Invalid dht node: no port number seperator ':'");
         }
         auto port_str = std::string_view(std::next(it), node.end());
         std::uint16_t port;
@@ -167,7 +190,8 @@ std::optional<std::size_t> parse_commandline_size(std::string_view option, const
     if (ec != std::errc{}) {
         throw CLI::ConversionError(fmt::format(err_msg, value, option, "expected integer"));
     }
-    auto suffix = std::string_view(ptr, (s.data()+s.size()-ptr));
+    auto suffix = std::string(ptr, (s.data()+s.size()-ptr));
+    trim(suffix);
 
     if (suffix == "k" || suffix == "ki" || suffix == "kib") {
         value *= 1024;
@@ -190,5 +214,140 @@ std::optional<std::size_t> parse_commandline_size(std::string_view option, const
                 fmt::format(err_msg, value, option, "must be larger or equal to 16 KiB"));
     }
     return value;
+}
 
+std::vector<std::optional<bool>> parse_commandline_booleans(std::string_view option, const std::vector<std::string>& v)
+{
+    std::vector<std::optional<bool>> result;
+
+    if (v.empty())
+        return {std::nullopt};
+
+    for (const auto& value : v) {
+        std::string lower_case_value;
+        rng::transform(value, std::back_inserter(lower_case_value), [](const char c) { return std::tolower(c); });
+        trim(lower_case_value);
+
+        if (value == "on" || value == "true" || value == "1") {
+            result.emplace_back(true);
+        }
+        if (value == "off" || value == "false" || value == "0") {
+            result.emplace_back(false);
+        }
+        result.emplace_back(std::nullopt);
+    }
+    return result;
+}
+
+std::optional<bool> parse_commandline_bool(std::string_view option, const std::vector<std::string>& v)
+{
+    auto flags = parse_commandline_booleans(option, v);
+    if (v.size() > 1) {
+        throw std::invalid_argument(fmt::format("error parsing {}: multiple values given", option));
+    }
+    if (v.empty()) {
+        throw std::invalid_argument(fmt::format("error parsing {}: no value given", option));
+    }
+    return flags.at(0);
+}
+
+// SOURCE: https://stackoverflow.com/questions/33421450/c-c-time-zone-correct-time-conversion-to-seconds-since-epoch/33438989#33438989
+std::chrono::minutes parse_offset(std::istream& in)
+{
+    using namespace std::chrono;
+    char c;
+    in >> c;
+    minutes result = 10*hours{c - '0'};
+    in >> c;
+    result += hours{c - '0'};
+    in >> c;
+    result += 10*minutes{c - '0'};
+    in >> c;
+    result += minutes{c - '0'};
+    return result;
+}
+
+
+std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+parse_utc_datetime(const std::string& v)
+{
+    using second_point = std::chrono::time_point<std::chrono::system_clock,
+            std::chrono::seconds>;
+
+    std::istringstream in(v);
+    in.exceptions(std::ios::failbit | std::ios::badbit);
+    int yi, mi, di;
+    char dash;
+    // check dash if you're picky
+    in >> yi >> dash >> mi >> dash >> di;
+    using namespace date;
+    auto ymd = year{yi}/mi/di;
+
+    // check ymd.ok() if you're picky
+    if (!ymd.ok()) {
+        throw std::invalid_argument("invalid year-month-date");
+    }
+
+    char T;
+    in >> T;
+
+    if (T != T) {
+        throw std::invalid_argument("expected 'T' after year-month-day");
+    }
+    // check T if you're picky
+    int hi, si;
+    char colon;
+    in >> hi >> colon >> mi >> colon >> si;
+
+    // check colon if you're picky
+    if (colon != ':') {
+        throw std::invalid_argument("expected colon after hours-minutes-seconds");
+    }
+    using namespace std::chrono;
+    auto h = hours{hi};
+    auto m = minutes{mi};
+    auto s = seconds{si};
+    second_point result = date::sys_days{ymd} + h + m + s;
+    char f;
+    in >> f;
+    if (f == '+')
+        result -= parse_offset(in);
+    else if (f == '-')
+        result += parse_offset(in);
+    else {
+        if (f != 'Z') {
+            throw std::invalid_argument("expected Z at the end of the input");
+        }
+    }
+    return result;
+}
+
+std::chrono::system_clock::time_point
+creation_date_transformer(std::string_view option, const std::vector<std::string>& v)
+{
+    using second_point = std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>;
+
+    if (v.empty())
+        throw std::invalid_argument("expected argument");
+
+    if (v.size() != 1)
+        throw std::invalid_argument("multiple options given.");
+
+    auto value = v.at(0);
+    std::uint64_t time_since_epoch;
+
+    // first try to parse as an integer
+    auto res = std::from_chars(value.data(), value.data()+value.size(), time_since_epoch);
+    if (res.ec != std::errc{}) {
+        auto msg = std::make_error_code(res.ec).message();
+        throw std::invalid_argument(fmt::format("error parsing {}: {}", option, msg));
+    }
+
+    if (res.ptr == value.data()+value.size())
+        return std::chrono::time_point<std::chrono::system_clock>(
+                std::chrono::seconds(time_since_epoch));
+
+    auto utc_time = parse_utc_datetime(v.at(0));
+    // cast to normal system_clock duration
+    return std::chrono::system_clock::time_point(utc_time);
 }
