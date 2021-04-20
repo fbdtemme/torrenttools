2c809725a5d052e19fbb9abc8f44014f2e0d184d
fbdtemme <floriandetemmerman@gmail.com>
2021-04-20 14:48:22 +0200

    Use png icon for appimage, fix missing semicolon in desktop.file

--- packages/appimage/build_appimage.sh
+++ packages/appimage/build_appimage.sh
@@ -5,8 +5,10 @@ APPDIR=$SOURCE_DIR/packages/appimage/AppDir
 BUILD_DIR="$SOURCE_DIR"/cmake-build-release
 
 # Copy icons file
-mkdir -p "$APPDIR/usr/share/icons/scalable/apps"
-cp "$SOURCE_DIR/resources/icons/scalable/torrenttools.svg" "$APPDIR/usr/share/icons/scalable/apps/"
+#mkdir -p "$APPDIR/usr/share/icons/scalable/apps"
+mkdir -p "$APPDIR/usr/share/icons/256x256/apps"
+#cp "$SOURCE_DIR/resources/icons/scalable/torrenttools.svg" "$APPDIR/usr/share/icons/scalable/apps/"
+cp "$SOURCE_DIR/resources/icons/256x256/torrenttools.png" "$APPDIR/usr/share/icons/256x256/apps/"
 
 # Copy desktop file
 mkdir -p "$APPDIR/usr/share/applications/"
