35fa5f7cca7156176712cc9f692b3eb9e1a3f113
fbdtemme <floriandetemmerman@gmail.com>
2021-07-31 15:46:59 +0200

    Add support for 8-bit color codes in LS_COLORS (#15)

---
+++ src/ls_colors.cpp
@@ -0,0 +1,271 @@
+//
+// Created by fbdtemme on 7/31/21.
+#include "ls_colors.hpp"
+
+namespace detail {
+
+constexpr tc::emphasis parse_emphasis(std::uint8_t graphics_attribute)
+{
+    switch (graphics_attribute) {
+    case 0  :  return tc::emphasis::reset;
+    case 1  :  return tc::emphasis::bold;
+    case 2  :  return tc::emphasis::faint;
+    case 3  :  return tc::emphasis::italic;
+    case 4  :  return tc::emphasis::underline;
+    case 5  :  return tc::emphasis::blinking;
+    case 7  :  return tc::emphasis::reverse;
+    case 9  :  return tc::emphasis::striketrough;
+    case 21 :  return tc::emphasis::double_underline;
+    case 53 :  return tc::emphasis::overline;
+    default :
+        throw std::invalid_argument(
+                fmt::format("no mapping from graphics attribute: {} to emphasis", graphics_attribute));
+    }
+}
+
+
+
+inline std::pair<tc::text_style, std::size_t>
+parse_color(std::string_view arguments)
+{
+    std::uint8_t int_value;
+    std::size_t offset = 0;
+
+    auto sep_it = rng::find(arguments, ';');
+    auto value_substr = std::string_view(arguments.begin() + offset, sep_it);
+
+    auto [p, ec] = std::from_chars(value_substr.begin(), value_substr.end(), int_value);
+    if (ec != std::errc{}) { throw std::invalid_argument("Error parsing color: not an integer value"); }
+
+    offset += value_substr.size();
+
+    bool is_background_color;
+    tc::color_type color;
+
+    if (30 <= int_value && int_value <= 37) {
+        is_background_color = false;
+        color = tc::terminal_color(int_value);
+    }
+    else if (40 <= int_value && int_value <= 47) {
+        is_background_color = true;
+        color = tc::terminal_color(int_value-10);
+    }
+    else {
+        if (int_value == 38) {
+            is_background_color = false;
+        }
+        if (int_value == 48) {
+            is_background_color = true;
+        }
+
+        if (arguments[offset] != ';') {
+            throw std::invalid_argument("invalid parameters seperator");
+        } else {
+            ++offset;
+        }
+
+        sep_it = rng::find(arguments.substr(offset), ';');
+        if (sep_it == arguments.end()) {
+            throw std::invalid_argument("expected value after alternate color mode selector");
+        }
+        value_substr = std::string_view(arguments.begin() + offset, sep_it);
+        auto [p, ec] = std::from_chars(value_substr.begin(), value_substr.end(), int_value);
+        if (ec != std::errc{}) { throw std::invalid_argument("Error parsing alternate color mode"); }
+        offset += value_substr.size() + 1;
+
+        // 256-bit colors
+        if (int_value == 5) {
+            sep_it = rng::find(arguments.substr(offset), ';');
+            value_substr = std::string_view(arguments.begin() + offset, sep_it);
+            auto [p, ec] = std::from_chars(value_substr.begin(), value_substr.end(), int_value);
+            if (ec != std::errc{}) { throw std::invalid_argument("Error parsing 256-bit color: not an integer value"); }
+            offset += value_substr.size();
+            color = tc::terminal_color_256(int_value);
+        }
+        else if (int_value == 2) {
+            auto [rgb_color, rgb_offset] = parse_rgb_color_component(arguments.substr(offset));
+            offset += rgb_offset;
+            color = rgb_color;
+        } else {
+            throw std::invalid_argument("unrecognised alternate color mode");
+        }
+    }
+
+    if (is_background_color) {
+        return std::make_pair(tc::bg(color), offset);
+    } else {
+        return std::make_pair(tc::fg(color), offset);
+    }
+}
+
+inline std::pair<tc::rgb_color, std::size_t>
+parse_rgb_color_component(std::string_view rgb_component)
+{
+    std::uint8_t r, g, b;
+    std::size_t offset = 0;
+
+    auto sep_it = rng::find(rgb_component.substr(offset), ';');
+    if (sep_it == rgb_component.end()) {
+        throw std::invalid_argument("expected 3 color mode values, only found 1");
+    }
+    auto value_substr = std::string_view(rgb_component.begin() + offset, sep_it);
+    auto [pr, ecr] = std::from_chars(value_substr.begin(), value_substr.end(), r);
+    if (ecr != std::errc{}) {
+        throw std::invalid_argument("Error parsing RGB color: R component is not an integer value");
+    }
+    offset += value_substr.size()+1;
+
+    sep_it = rng::find(rgb_component.substr(offset), ';');
+    if (sep_it == rgb_component.end()) {
+        throw std::invalid_argument("expected 3 color mode values, only found 2");
+    }
+    value_substr = std::string_view(rgb_component.begin() + offset, sep_it);
+    auto [pg, ecg] = std::from_chars(value_substr.begin(), value_substr.end(), g);
+    if (ecg != std::errc{}) {
+        throw std::invalid_argument("Error parsing RGB color: G component is not an integer value");
+    }
+    offset += value_substr.size()+1;
+
+    sep_it = rng::find(rgb_component.substr(offset), ';');
+    value_substr = std::string_view(rgb_component.begin() + offset, sep_it);
+    auto [pb, ecb] = std::from_chars(value_substr.begin(), value_substr.end(), b);
+    if (ecb != std::errc{}) {
+        throw std::invalid_argument("Error parsing RGB color: B component is not an integer value");
+    }
+    offset += value_substr.size();
+
+    return std::make_pair(tc::rgb_color(r, g, b), offset);
+}
+
+
+/// Parse the LS_COLORS string into a map of text_styles.
+inline std::map<std::string, termcontrol::text_style>
+parse_ls_colors(std::string_view ls_colors)
+{
+    using namespace std::string_literals;
+    std::map<std::string, termcontrol::text_style> ls_style_map {};
+
+    std::size_t ls_offset = 0, pos = 0, idx = 0, sep_prev = 0, sep = 0;
+    std::uint8_t emphasis;
+    std::uint8_t foreground;
+    std::uint8_t background;
+
+    while ((pos = ls_colors.find(':', ls_offset)) != std::string_view::npos) {
+        auto entry = ls_colors.substr(ls_offset, pos-ls_offset);
+        std::size_t equal_sign_pos = entry.find('=');
+        auto key = entry.substr(0, equal_sign_pos);
+        auto value = entry.substr(equal_sign_pos+1);
+
+        termcontrol::text_style style;
+        std::size_t offset = 0;
+        auto sep = value.substr(offset).find(';');
+
+        while (offset < value.size())
+        {
+            std::uint8_t int_value;
+            auto part_str = value.substr(offset, sep);
+
+            auto [p, ec] = std::from_chars(part_str.begin(), part_str.end(), int_value);
+
+            // attribute is color or start of a color
+            if (30 <= int_value && int_value <= 49
+            || 90 <= int_value && int_value <= 97
+            || 100 < int_value && int_value <= 107 )
+            {
+                auto res = parse_color(value.substr(offset));
+                style |= res.first;
+                offset += res.second;
+            }
+            else {
+                auto res = parse_emphasis(int_value);
+                style |= tc::em(res);
+                offset += part_str.size();
+            }
+
+            if (offset < value.size()) {
+                if (value[offset] == ';') { ++offset; }
+                else { throw std::invalid_argument("invalid seperator"); }
+            }
+        }
+
+        ls_style_map.insert_or_assign(std::string(key), style);
+        ls_offset = pos;
+
+        if (ls_offset < ls_colors.size()) {
+            if (ls_offset == pos) { ++ls_offset; }
+            else {
+                throw std::invalid_argument("invalid entry seperator");
+            }
+            pos = ls_colors.find(':', ls_offset);
+        } else {
+            pos = std::string_view::npos;
+        }
+    }
+
+    return ls_style_map;
+}
+
+
+} // namespace detail
+
+ls_colors::ls_colors()
+        : data_()
+        , extensions_()
+{
+    load_system_colors();
+}
+
+termcontrol::text_style ls_colors::file_style(const dottorrent::file_entry& entry) const {
+    if (entry.is_symlink()) {
+        return data_.link;
+    }
+    else if (entry.is_executable()) {
+        return data_.exec;
+    }
+    else {
+        if (entry.path().has_extension()) {
+            const auto& ext = entry.path().extension().string();
+
+            // Prefer an extension
+            if (auto it = extensions_.find(ext); it != extensions_.end()) {
+                return it->second;
+            }
+        }
+
+        return data_.normal;
+    }
+}
+
+termcontrol::text_style ls_colors::directory_style() const {
+    return data_.dir;
+}
+
+
+bool ls_colors::load_system_colors() noexcept {
+    const char* lc_colors_p = std::getenv("LS_COLORS");
+    if (!lc_colors_p)
+        return false;
+
+    try {
+        auto map = detail::parse_ls_colors(lc_colors_p);
+
+        data_.normal                = map["no"];
+        data_.file                  = map["fi"];
+        data_.dir                   = map["di"];
+        data_.link                  = map["ln"];
+        data_.exec                  = map["ex"];
+
+        for (const auto& [k, v] : map) {
+            if (k.starts_with('*')) {
+                // strip * from key.
+                // We keep the dot since fs::path::extension() includes the dot.
+                extensions_.emplace(k.substr(1), v);
+            }
+        }
+        return true;
+    }
+    catch (const std::exception& e) {
+        return false;
+    }
+}
+
---
+++ tests/test_ls_colors.cpp
@@ -0,0 +1,98 @@
+
+#include <catch2/catch.hpp>
+#include "ls_colors.hpp"
+
+TEST_CASE("test parse_color")
+{
+    SECTION("terminal color - foreground") {
+        auto input = "35";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_foreground_color() == tc::terminal_color(35));
+    }
+    SECTION("terminal color - background") {
+        auto input = "43";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_background_color() == tc::terminal_color(33));
+    }
+    SECTION("256-bit terminal color - foreground") {
+        auto input = "38;5;156";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_foreground_color() == tc::terminal_color_256(156));
+    }
+    SECTION("256-bit terminal color - background") {
+        auto input = "48;5;156";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_background_color() == tc::terminal_color_256(156));
+    }
+    SECTION("RGB color -foreground") {
+        auto input = "38;2;4;194;130";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_foreground_color() == tc::rgb_color(4, 194, 130));
+    }
+    SECTION("RGB color - background") {
+        auto input = "48;2;4;194;130";
+        auto [c, it] = detail::parse_color(input);
+        CHECK(c.get_background_color() == tc::rgb_color(4, 194, 130));
+    }
+    SECTION("missing color after alternate color mode") {
+        auto input1 = "38;5";
+        CHECK_THROWS(detail::parse_color(input1));
+        auto input2 = "38;2";
+        CHECK_THROWS(detail::parse_color(input2));
+    }
+    SECTION("missing rgb components") {
+        auto input1 = "38;2;13";
+        CHECK_THROWS(detail::parse_color(input1));
+        auto input2= "38;2;13;234";
+        CHECK_THROWS(detail::parse_color(input2));
+    }
+    SECTION("invalid terminal color - out of range") {
+        auto input = "1243";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid 256-bit terminal color - out of range") {
+        auto input = "38;5;1243";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid RGB color - out of range") {
+        auto input = "38;2;20;156;399";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid RGB color - missing red component") {
+        auto input = "38;2";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid RGB color - missing green component") {
+        auto input = "38;2;20;";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid RGB color - missing blue component") {
+        auto input = "38;2;20;156";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("invalid argument seperator") {
+        auto input = "38|40";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+    SECTION("expected value after alternate color mode") {
+        auto input = "38;5";
+        CHECK_THROWS(detail::parse_color(input));
+    }
+}
+
+
+TEST_CASE("test parse_ls_colors - 256 bit colors")
+{
+    SECTION("default") {
+        auto input = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
+        detail::parse_ls_colors(input);
+    }
+    SECTION("8 bit colors") {
+        auto input = "rs=0:di=38;5;27:ln=38;5;51:mh=44;38;5;15:pi=40;38;5;11:so=38;5;13:do=38;5;5:bd=48;5;232;38;5;11:cd=48;5;232;38;5;3:or=48;5;232;38;5;9:mi=05;48;5;232;38;5;15:su=48;5;196;38;5;15:sg=48;5;11;38;5;16:ca=48;5;196;38;5;226:tw=48;5;10;38;5;16:ow=48;5;10;38;5;21:st=48;5;21;38;5;15:ex=38;5;34:*.tar=38;5;9:*.tgz=38;5;9:*.arc=38;5;9:*.arj=38;5;9:*.taz=38;5;9:*.lha=38;5;9:*.lz4=38;5;9:*.lzh=38;5;9:*.lzma=38;5;9:*.tlz=38;5;9:*.txz=38;5;9:*.tzo=38;5;9:*.t7z=38;5;9:*.zip=38;5;9:*.z=38;5;9:*.Z=38;5;9:*.dz=38;5;9:*.gz=38;5;9:*.lrz=38;5;9:*.lz=38;5;9:*.lzo=38;5;9:*.xz=38;5;9:*.bz2=38;5;9:*.bz=38;5;9:*.tbz=38;5;9:*.tbz2=38;5;9:*.tz=38;5;9:*.deb=38;5;9:*.rpm=38;5;9:*.jar=38;5;9:*.war=38;5;9:*.ear=38;5;9:*.sar=38;5;9:*.rar=38;5;9:*.alz=38;5;9:*.ace=38;5;9:*.zoo=38;5;9:*.cpio=38;5;9:*.7z=38;5;9:*.rz=38;5;9:*.cab=38;5;9:*.jpg=38;5;13:*.jpeg=38;5;13:*.gif=38;5;13:*.bmp=38;5;13:*.pbm=38;5;13:*.pgm=38;5;13:*.ppm=38;5;13:*.tga=38;5;13:*.xbm=38;5;13:*.xpm=38;5;13:*.tif=38;5;13:*.tiff=38;5;13:*.png=38;5;13:*.svg=38;5;13:*.svgz=38;5;13:*.mng=38;5;13:*.pcx=38;5;13:*.mov=38;5;13:*.mpg=38;5;13:*.mpeg=38;5;13:*.m2v=38;5;13:*.mkv=38;5;13:*.webm=38;5;13:*.ogm=38;5;13:*.mp4=38;5;13:*.m4v=38;5;13:*.mp4v=38;5;13:*.vob=38;5;13:*.qt=38;5;13:*.nuv=38;5;13:*.wmv=38;5;13:*.asf=38;5;13:*.rm=38;5;13:*.rmvb=38;5;13:*.flc=38;5;13:*.avi=38;5;13:*.fli=38;5;13:*.flv=38;5;13:*.gl=38;5;13:*.dl=38;5;13:*.xcf=38;5;13:*.xwd=38;5;13:*.yuv=38;5;13:*.cgm=38;5;13:*.emf=38;5;13:*.axv=38;5;13:*.anx=38;5;13:*.ogv=38;5;13:*.ogx=38;5;13:*.aac=38;5;45:*.au=38;5;45:*.flac=38;5;45:*.mid=38;5;45:*.midi=38;5;45:*.mka=38;5;45:*.mp3=38;5;45:*.mpc=38;5;45:*.ogg=38;5;45:*.ra=38;5;45:*.wav=38;5;45:*.axa=38;5;45:*.oga=38;5;45:*.spx=38;5;45:*.xspf=38;5;45";
+        detail::parse_ls_colors(input);
+    }
+    SECTION("24 bit colors") {
+        auto input = "di=1;38;2;69;133;136";
+        detail::parse_ls_colors(input);
+    }
+}
